03e51523dfb3eac6a64e969372ca14ba0acc2653
Sneha Agarwalla <disneha@gmail.com>
2019-01-08 16:52:20 +0530

    1. Smoke Test

---
+++ contracts/MyToken.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+contract MyToken {
+	uint256 public totalSupply;
+	//contructor
+	constructor () public {
+		totalSupply = 1000000;
+	}
+
+	//set the total number of tokens
+	//read the total number of tokens
+
+}
