a90d05d5b0081b7356efb263ba960a720b0211a2
Sneha Agarwalla <disneha@gmail.com>
2019-01-08 21:11:32 +0530

    3. Delegated Transfers

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -6,12 +6,20 @@ contract MyToken {
 	string public standard = "My Token v1.0";
 	uint256 public totalSupply;
 
+	event Approval(
+		address indexed _owner,
+		address indexed _to,
+		uint256 _value);
+
 	event Transfer(
 		address indexed _from, 
 		address indexed _to,
 		uint256 _value);
 
 	mapping (address => uint256) public balanceOf;
+
+	//allowance
+	mapping (address => mapping (address => uint256)) public allowance;
 	
 	constructor (uint256 _initialSupply) public {
 		balanceOf[msg.sender] = _initialSupply;
@@ -32,6 +40,42 @@ contract MyToken {
 		//return boolean
 		return true;
 
+	}
+	
+	//delegated transfer
+
+	//approve
+	function approve (address _spender, uint256 _value) public returns(bool success) {
+		//allowance
+		allowance[msg.sender][_spender] = _value;
+		//approval event
+		emit Approval(msg.sender, _spender, _value);
+
+		return true;
+	}
+	
+
+	//transferFrom
+	function transferFrom (address _from, address _to, uint256 _value) public returns(bool success) {
+		
+		//require _from has enuf tokens
+		require (_value <= balanceOf[_from]);
+		
+		//requre allowance is big enuf
+		require (_value <= allowance[_from][msg.sender]);
+		
+		//change the balance
+		balanceOf[_from] -= _value;
+		balanceOf[_to] += _value;
+
+		//update the allowance
+		allowance[_from][msg.sender] -= _value;
+
+		//transfer event 
+		emit Transfer(_from, _to, _value);
+
+		//return a boolean
+		return true;
 	}
 	
 
